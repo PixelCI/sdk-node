d9a0d64fde7e5a42f0d187d9ab2b7b5254fd95a1
Giacomo Tagliabue <giacomo.tag@gmail.com>
2018-03-18 15:50:15 -0400

    [WIP] Add initial implementation (#1)

---
+++ src/client.ts
@@ -0,0 +1,31 @@
+import * as fetch from 'node-fetch'
+
+const API_URL = 'https://broserci.io/graphql'
+
+export default class Client {
+  constructor(private apiKey: string) {}
+
+  async request(query: string) {
+    const resp = await fetch(API_URL, {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    })
+
+    if (!resp.ok) {
+      const text = await resp.text()
+      throw new Error(`browserci API Error: \n${text}`)
+    }
+
+    const { data, errors } = await resp.json()
+    if (errors) {
+      throw new Error(`browserci API Error: \n${JSON.stringify(errors)}`)
+    }
+
+    return data
+  }
+}
