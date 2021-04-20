20546b413c3fb62365758590aba38d8340b7d9c5
Giacomo Tagliabue <giacomo.tag@gmail.com>
2021-04-20 10:34:16 -0400

    revamp

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,21 @@
-import { mixed, Type } from 'io-ts'
-import { ThrowReporter } from 'io-ts/lib/ThrowReporter'
+import { Type, mixed } from 'io-ts';
+import { ThrowReporter } from 'io-ts/lib/ThrowReporter';
 
-export type ValidateEntry = [Type<any>, mixed]
+export type ValidateEntry = [Type<any>, mixed];
 
 export function validateArgs(args: { [idx: string]: ValidateEntry }) {
-  Object.keys(args).forEach(argName => {
-    const [type, input] = args[argName]
-    const value = type.decode(input)
+  Object.keys(args).forEach((argName) => {
+    const [type, input] = args[argName];
+    const value = type.decode(input);
     try {
-      ThrowReporter.report(value)
+      ThrowReporter.report(value);
     } catch (e) {
-      throw new Error(`bad input for argument ${argName}:\n${e}`)
+      throw new Error(`bad input for argument ${argName}:\n${e}`);
     }
-  })
+  });
 }
 
 // just a stupid utility function for prettier indentation
-export function gql(strings) {
-  return strings[0]
+export function gql(strings: TemplateStringsArray) {
+  return strings[0];
 }
